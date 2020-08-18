a773cb2980adb5a2c1fe3f88659398c7dab991bb
Mihai Todor <todormihai@gmail.com>
2020-08-18 13:01:00 +0100

    Add VS Code settings and extensions

---
+++ vscode/install_extensions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+code --install-extension eamodio.gitlens
+code --install-extension euskadi31.json-pretty-printer
+code --install-extension golang.go
+code --install-extension hashicorp.terraform
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension mtxr.sqltools
+code --install-extension mushan.vscode-paste-image
+code --install-extension redhat.vscode-yaml
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension zxh404.vscode-proto3

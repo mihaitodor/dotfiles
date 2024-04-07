6568ad02910e8224ab42d846f159204fe174836a
Mihai Todor <todormihai@gmail.com>
2024-04-07 20:28:12 +0100

    Update VS Code settings

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Created with the following command:
+# > code --list-extensions | xargs -L 1 echo code --install-extension
+
 code --install-extension eamodio.gitlens
 code --install-extension euskadi31.json-pretty-printer
+code --install-extension georgeshaidar.vsc-benthos
 code --install-extension golang.go
-code --install-extension hashicorp.terraform
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.autopep8
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.flake8
+code --install-extension ms-python.isort
 code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension mtxr.sqltools
-code --install-extension mushan.vscode-paste-image
-code --install-extension redhat.vscode-yaml
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.makefile-tools
 code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension zxh404.vscode-proto3
